066e169681c86842968cae837dbab1e7ec3d104c
Sergkochetkov3224 <artemferlikin@gmail.com>
2025-03-18 18:53:56 +0300

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,3 +4,15 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Привет, Пупсик! Сайт работает! 🚀")
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", home),  # Добавляем главную страницу
+]
