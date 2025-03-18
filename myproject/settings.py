a5a668a73c281f47a79110415e330a7f6fc702c8
Sergkochetkov3224 <artemferlikin@gmail.com>
2025-03-18 19:24:15 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "your-default-secret-key")
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
